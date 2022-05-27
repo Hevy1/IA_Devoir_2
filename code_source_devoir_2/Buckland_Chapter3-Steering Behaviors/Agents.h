3bc2bee82a98aaaf98524cf87194abdbecd23151
JorickC <jorick.celarier42@gmail.com>
2022-05-27 15:53:12 -0400

    Ajout classe Follower

--- code_source_devoir_2/Buckland_Chapter3-Steering Behaviors/Agents.h
+++ code_source_devoir_2/Buckland_Chapter3-Steering Behaviors/Agents.h
@@ -1,7 +1,19 @@
+#ifndef AGENTS_H
+#define AGENTS_H
+
 #include "Vehicle.h"
+#include "SteeringBehaviors.h"
+
 
 class Follower : public Vehicle
 {
+private :
+
+
+
+public :
+
+	Follower::Follower(GameWorld* world, Vector2D position, double mass, double max_force, double max_speed, double max_turn_rate, double scale);
 
 };
 
@@ -10,3 +22,5 @@ class Leader : public Vehicle
 {
 
 };
+
+#endif
