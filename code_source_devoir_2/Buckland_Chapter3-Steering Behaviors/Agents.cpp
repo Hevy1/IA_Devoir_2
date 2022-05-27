706e3b065388a3defc39ced1be43c72d2719dba0
Hevy1 <evan.weisse@outlook.fr>
2022-05-27 15:53:22 -0400

    Ajout classe Leader

---
+++ code_source_devoir_2/Buckland_Chapter3-Steering Behaviors/Agents.cpp
@@ -0,0 +1,27 @@
+#include "Agents.h"
+#include "SteeringBehaviors.h"
+
+
+// Functions related to Leader class
+Leader::Leader(GameWorld* world,
+    Vector2D position,
+    double    rotation,
+    Vector2D velocity,
+    double    mass,
+    double    max_force,
+    double    max_speed,
+    double    max_turn_rate,
+    double    scale) : Vehicle(world,
+        position,
+        rotation,
+        velocity,
+        mass,
+        max_force,
+        max_speed,
+        max_turn_rate,
+        scale)
+{
+    this->SetScale(Vector2D(10, 10));
+    this->Steering()->WanderOn();
+    this->SetMaxSpeed(70);
+}
--- code_source_devoir_2/Buckland_Chapter3-Steering Behaviors/Agents.h
+++ code_source_devoir_2/Buckland_Chapter3-Steering Behaviors/Agents.h
@@ -8,5 +8,15 @@ class Follower : public Vehicle
 
 class Leader : public Vehicle
 {
-
+public:
+	Leader(GameWorld* world,
+        Vector2D position,
+        double    rotation,
+        Vector2D velocity,
+        double    mass,
+        double    max_force,
+        double    max_speed,
+        double    max_turn_rate,
+        double    scale);
+	~Leader();
 };
